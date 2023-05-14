4652607f21f29d6e485d24c85eeb8371c041e3ee
Javed <50339361+JavedHussain-06@users.noreply.github.com>
2023-05-14 18:05:08 +0530

    updated getStaticPath

--- pages/events/[cat]/index.js
+++ pages/events/[cat]/index.js
@@ -1,16 +1,18 @@
 import CatEvents from '../../../src/Components/Events/catEvents'
 
 export async function getStaticPaths() {
-  // fetch categories from your data source
-  const categories = await import('/data/data.json'); // assuming this returns an array of category objects
-
-  // create an array of paths based on the categories
-  const paths = categories.events_categories.map(category => ({
-    params: { cat: category.id }
-  }));
-
-  // return the paths to Next.js
-  return { paths, fallback: false };
+  const { events_categories } = await import('/data/data.json');
+  const allPaths = events_categories.map((ev) => {
+    return {
+      params: {
+        cat: ev.id.toString(),
+      },
+    };
+  });
+  return {
+    paths: allPaths,
+    fallback: false,
+  };
 }
 
 export async function getStaticProps(context){
